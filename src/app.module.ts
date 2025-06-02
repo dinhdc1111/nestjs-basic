c2d43067a1228077bc56c55c29a5187565269f46
Cong Dinh <dinhdc1111@gmail.com>
2025-06-02 21:38:21 +0700

    feat: :sparkles: .env with configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://dinhdc1111:6E%40.4dwwpfqdFrr@cluster0.sjpibu5.mongodb.net/',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useStaticAssets(join(__dirname, '..', 'public')); // Static assets: css, images, js
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // Views directory for EJS templates
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(configService.get<string>("PORT"));
 }
 bootstrap();
