bed51f0766b8c24a54785d7142e4102ef55e52c3
Cong Dinh <dinhdc1111@gmail.com>
2025-06-04 00:40:59 +0700

    feat: :sparkles: create a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,8 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body("email") email: string, @Body("password") password: string,) {
+    return this.usersService.create({ email, password });
   }
 
   @Get()
